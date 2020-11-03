c6dd447d5f99e362bd4f0d244746fccd966c0ab2
Rahul <rahul@Rahuls-MacBook-Pro.local>
2020-11-03 15:12:51 +0800

    Changing variable names to make more sense

--- ansible_files/delete_old.sh
+++ ansible_files/delete_old.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#This script assumes that keep_part is greater than keep whole
-keep_whole=7
-keep_part=30
-whole=(debug_log)
-part=(debug_log)
-for i in $(seq 0 $keep_part); do
+#This script assumes that del_old_than is greater than mod_old_than
+del_old_than=30
+mod_old_than=7
+not_del=(debug_log)
+not_mod=(debug_log)
+for i in $(seq 0 $del_old_than); do
 	curdate=$(date --date "today - $i day" "+%Y_%m_%d")
-	part+=($curdate)
-	if [ $keep_whole -ge $i ]; then
-		whole+=($curdate)
+	not_del+=($curdate)
+	if [ $mod_old_than -ge $i ]; then
+		not_mod+=($curdate)
 	fi
 done
 
 # Delete old files
-find ~/logs_weather -type f $(printf "! -name %s " ${part[*]}) -exec rm -v {} \;
+find ~/logs_weather -type f $(printf "! -name %s " ${not_del[*]}) -exec rm -v {} \;
 
 #Modify 
-files=$(find log_folder/ -type f $(printf "! -name %s " ${whole[*]}))
+files=$(find log_folder/ -type f $(printf "! -name %s " ${not_mod[*]}))
 for file in ${files[@]}; do
 	echo Processing file $file
 	tail -n 1 $file > tempfile
